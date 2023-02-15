1c0fa0bf5520516657883307729ba20ccd620813
Hariom1509 <hariomvyas1509@gmail.com>
2023-02-15 18:45:54 +0530

    Frontend

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,5 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: 'https://polygon-mainnet.g.alchemy.com/v2/nqBl6KQB2xMolO79psPuCgE7RFXOA31u',
+      accounts: ['6ceec19ba7d6b0436ac30675c0ba4003e0d048653dc24ce472541ad968673157'],
+    }
+  },
   solidity: {
     version: '0.8.9',
     settings: {
---
+++ client/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ThirdwebProvider desiredChainId={ChainId.Polygon}>
+        <Router>
+            <App />
+        </Router>
+    </ThirdwebProvider>
+)
